efa2aed97397cf37eb4f4fdb6241c4113fd6f831
Brian Hild <hild.brian@gmail.com>
2016-06-29 10:46:42 -0500

    Initial Code Push
    Pushing the initial project and code

---
+++ Acres Craft CannabisTests/Acres_Craft_CannabisTests.swift
@@ -0,0 +1,36 @@
+//
+//  Acres_Craft_CannabisTests.swift
+//  Acres Craft CannabisTests
+//
+//  Created by Brian Hild on 6/29/16.
+//  Copyright © 2016 Demonic Penguin. All rights reserved.
+//
+
+import XCTest
+@testable import Acres_Craft_Cannabis
+
+class Acres_Craft_CannabisTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
